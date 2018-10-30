b771abb27ba5f9e0a2055570d5d27c1d293b5eac
Roberto Arcomano <info@robertoarcomano.it>
2018-10-30 13:24:45 +0100

    First commit: playbook with tasks,handlers and vars on single host

---
+++ test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# 0. Variables
+# HOST=rb2
+HOST=ubuntu_ssh1
+HOSTSPATH="-i ./hosts"
+PARAMS="$HOST $HOSTSPATH"
+
+# 1. Launch ping command on $HOST
+# ansible $HOST -m ping
+
+# 2. Launch a command, uptime
+# ansible $HOST -m command -a uptime
+ansible $PARAMS -a uptime
+
+# 3. Launch a command, read syslog
+# ansible $PARAMS -a "tail /var/log/syslog"
+
+# 4. Launch a command, check user
+# ansible $PARAMS -a "id"
+
+# 5. Install nginx
+# ansible $PARAMS -m apt -a "name=nginx update_cache=yes" -vvvv
+
+# 6. Start nginx
+# ansible $PARAMS -m service -a "name=nginx state=started" -vvvv
+
+# 7. Group Execution
+# ansible $HOSTSPATH webservers -m ping
+
+# 8. Playbooks
+ansible-playbook $HOSTSPATH playbook1.yml
